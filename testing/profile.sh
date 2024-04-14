bcc5b133a87a0e91e3b144ecbf819f2a4ef0c3b8
AlexandreSinger <alex.singer@mail.utoronto.ca>
2024-04-14 13:02:25 -0400

    [Testing] Updated Bash Script
    Updated the bash script to allow it to change which test suite is
    currently being run.

--- testing/profile.sh
+++ testing/profile.sh
@@ -1,28 +1,29 @@
 
 set -x
 
+TEST_SUITE=bwave_like
 
 export MQ_NUM_THREADS=1
 export MQ_NUM_QUEUES_PER_THREAD=4
-./run_test.py bwave_like -j1
+./run_test.py $TEST_SUITE -j1
 
 export MQ_NUM_THREADS=2
 export MQ_NUM_QUEUES_PER_THREAD=4
-./run_test.py bwave_like -j1
+./run_test.py $TEST_SUITE -j1
 
 export MQ_NUM_THREADS=3
 export MQ_NUM_QUEUES_PER_THREAD=4
-./run_test.py bwave_like -j1
+./run_test.py $TEST_SUITE -j1
 
 export MQ_NUM_THREADS=6
 export MQ_NUM_QUEUES_PER_THREAD=4
-./run_test.py bwave_like -j1
+./run_test.py $TEST_SUITE -j1
 
 export MQ_NUM_THREADS=12
 export MQ_NUM_QUEUES_PER_THREAD=4
-./run_test.py bwave_like -j1
+./run_test.py $TEST_SUITE -j1
 
 export MQ_NUM_THREADS=24
 export MQ_NUM_QUEUES_PER_THREAD=4
-./run_test.py bwave_like -j1
+./run_test.py $TEST_SUITE -j1
 
